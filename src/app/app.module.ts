d1e16cf376f67c13916f18deac643034c2a171bb
Matthew Davis <matthew@matthewdavis.io>
2018-10-29 11:55:11 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AutoFocusDirective} from './auto-focus.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+    declarations: [
+        AppComponent,
+        AutoFocusDirective
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+}
